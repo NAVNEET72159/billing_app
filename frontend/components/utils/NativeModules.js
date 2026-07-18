8e5cdf74d9ece13ca3b6a86e605e6456ef833d03
NAVNEET72159 <navneetkumarroy5740084@gmail.com>
2026-07-18 19:29:16 +0530

    updates

--- frontend/components/utils/NativeModules.js
+++ frontend/components/utils/NativeModules.js
@@ -1,4 +1,14 @@
 import { captureRef } from 'react-native-view-shot';
 import * as MediaLibrary from 'expo-media-library';
 
+captureRef = null;
+MediaLibrary = null;
+
+try {
+    captureRef = require('react-native-view-shot').captureRef;
+    MediaLibrary = require('expo-media-library');
+} catch (error) {
+    console.warn("Native modules are compiling...");
+}
+
 export { captureRef, MediaLibrary };
